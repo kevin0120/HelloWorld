fc62276c92c8a08c19d3e66d177e18066469c563
kevin <jingwei1224@163.com>
2020-11-16 20:28:46 +0800

    update time about

--- testTime/main.go
+++ testTime/main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 
 	dateString := "2019-10-16T11:20:30+08:00"
+	dateString2 := "2019-10-16 11:20:30"
+
 
 	dt := time.Now()
 	fmt.Println(dt)
@@ -17,14 +19,31 @@ func main() {
 	fmt.Println(dt.Local())
 	fmt.Println(dt.Location())
 
+	if dateString2 != "" {
+		dt11, _ := time.Parse("2006-01-02 15:04:05", dateString2)
+		fmt.Println("dt11",dt11)
+	}
+
+	if dateString != "" {
+		dt1, _ := time.Parse(time.RFC3339, dateString)
+		fmt.Println(dt1)
+	}
+
+
 	if dateString != "" {
 		loc, _ := time.LoadLocation("Local")
 		dt, _ = time.ParseInLocation(time.RFC3339, dateString, loc)
+		dt12, _ := time.ParseInLocation("2006-01-02 15:04:05", dateString2, loc)
+		fmt.Println("dt12",dt12)
 	}
 
 	//UpdateTime := dt.UTC()
 	fmt.Println(dt)
 	fmt.Println(dt.Format(time.RFC3339))
+
+
+	fmt.Println("special time",time.Now().Format("06010215 04"))
+
 	//指定时间
 	fmt.Println(time.Unix(11, 01).UTC())
 
