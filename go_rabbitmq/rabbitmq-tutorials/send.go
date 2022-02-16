50dc469dd828d8632bbbf953942f45146753a382
kevin0120 <jingwei1224@163.com>
2022-02-16 14:28:17 +0800

    rabbitmq

--- go_rabbitmq/rabbitmq-tutorials/send.go
+++ go_rabbitmq/rabbitmq-tutorials/send.go
@@ -13,7 +13,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://admin:admin@localhost:5672")
+	conn, err := amqp.Dial("amqp://admin:admin@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
