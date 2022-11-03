d814631d81a8e46a5451aafe0f4d48d2bcb33c3b
kevin0120 <jingwei1224@163.com>
2022-11-03 15:01:50 +0800

    client

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kevin0120/HelloWorld
 go 1.16
 
 require (
+	github.com/StackExchange/wmi v1.2.1
 	github.com/alibabacloud-go/darabonba-openapi v0.1.16
 	github.com/alibabacloud-go/dysmsapi-20170525/v2 v2.0.9
 	github.com/alibabacloud-go/tea v1.1.17
--- testTCP/rrr/main.go
+++ testTCP/rrr/main.go
@@ -28,7 +28,7 @@ func main() {
 	//	fmt.Fprintf(os.Stderr, "用法:%s ip地址\n", os.Args[0])
 	//	os.Exit(1)
 	//}
-	service := "10.1.1.54:8000"
+	service := "192.168.60.157:8000"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkError(err)
 
@@ -37,6 +37,9 @@ func main() {
 	// fmt.Println("远程地址:")
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
+	_, err = conn.Write([]byte("GGGGGGGGGGGGGG"))
+	testConnectClient(conn)
+
 	//conn.SetReadBuffer(0)
 	//fmt.Println(conn)err
 	//go func() {
@@ -47,73 +50,85 @@ func main() {
 	//
 	//
 	//
-	go func() {
-		var msr [512]byte
-		for {
-			n, err := conn.Read(msr[0:])
-			if err != nil {
-				fmt.Printf("%v read error :%s \n", time.Now(), err)
-				conn.Close()
-				break
-			} else {
-				fmt.Printf("%v recieve :%s \n", time.Now(), string(msr[0:n]))
-			}
-		}
-	}()
-
-	//time.Sleep(5 * time.Second)
+	//go func() {
+	//	var msr [512]byte
+	//	for {
+	//		n, err := conn.Read(msr[0:])
+	//		if err != nil {
+	//			fmt.Printf("%v read error :%s \n", time.Now(), err)
+	//			conn.Close()
+	//			break
+	//		} else {
+	//			fmt.Printf("%v recieve :%s \n", time.Now(), string(msr[0:n]))
+	//		}
+	//	}
+	//}()
+	//
+	//time.Sleep(70 * time.Second)
 	//conn.Close()
 
-	i := 0
-	for {
-		//var msr [512]byte
-		//////
-		/////*
-		////	_, err = conn.Write([]byte("GGGGGGGGGGGGGG"))
-		////	checkError(err)
-		////*/
-		//////result, err := ioutil.ReadAll(conn)
-		//////fmt.Println(string(msr[0]))
-		//////os.Exit(0)
-		////go func() {
-		//	_, err = conn.Read(msr[0:])
-		//	if err != nil {
-		//		fmt.Printf("%v read error :%s \n", time.Now(), err)
-		//	} else {
-		//		fmt.Printf("%v recieve :%s \n", time.Now(), string(msr[0:]))
-		//	}
-		//}()
-		//
-		//
-		//
-		//_, err = conn.Write([]byte("hello"))
-		_, err = conn.Write([]byte(fmt.Sprintf("hello%5d", i)))
-		i++
-		if err != nil {
-			fmt.Printf("%v write error :%s \n", time.Now(), err)
-			conn.Close()
-			break
-		}
-		//conn.Close()
-		//if err != nil {
-		//	fmt.Printf("%v write11111 error :%s \n", time.Now(), err)
-		//	if err, ok := err.(net.Error); !ok || !err.Temporary() {
-		//		// permanent error. close the connection
-		//		conn.Close()
-		//		//conn= nil
-		//		fmt.Printf("%v write22222 error :%s \n", time.Now(), err)
-		//	}
-		//}
-		time.Sleep(3000 * time.Millisecond)
-		//_, err = conn.Write(nil)
-
-	}
-	//err := conn.Close()
+	//i := 0
+	//for {
+	//	//var msr [512]byte
+	//	//////
+	//	/////*
+	//	////	_, err = conn.Write([]byte("GGGGGGGGGGGGGG"))
+	//	////	checkError(err)
+	//	////*/
+	//	//////result, err := ioutil.ReadAll(conn)
+	//	//////fmt.Println(string(msr[0]))
+	//	//////os.Exit(0)
+	//	////go func() {
+	//	//	_, err = conn.Read(msr[0:])
+	//	//	if err != nil {
+	//	//		fmt.Printf("%v read error :%s \n", time.Now(), err)
+	//	//	} else {
+	//	//		fmt.Printf("%v recieve :%s \n", time.Now(), string(msr[0:]))
+	//	//	}
+	//	//}()
+	//	//
+	//	//
+	//	//
+	//	//_, err = conn.Write([]byte("hello"))
+	//	_, err = conn.Write([]byte(fmt.Sprintf("hello%5d", i)))
+	//	i++
+	//	if err != nil {
+	//		fmt.Printf("%v write error :%s \n", time.Now(), err)
+	//		conn.Close()
+	//		break
+	//	}
+	//	//conn.Close()
+	//	//if err != nil {
+	//	//	fmt.Printf("%v write11111 error :%s \n", time.Now(), err)
+	//	//	if err, ok := err.(net.Error); !ok || !err.Temporary() {
+	//	//		// permanent error. close the connection
+	//	//		conn.Close()
+	//	//		//conn= nil
+	//	//		fmt.Printf("%v write22222 error :%s \n", time.Now(), err)
+	//	//	}
+	//	//}
+	//	time.Sleep(3000 * time.Millisecond)
+	//	//_, err = conn.Write(nil)
+	//
+	//}
+	////err := conn.Close()
 
 }
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "cuowu:%s", err.Error())
 		os.Exit(1)
+	}
+}
+
+func testConnectClient(conn *net.TCPConn) {
+	err1 := conn.SetKeepAlive(false)
+	if err1 != nil {
+		return
+	}
+	time.Sleep(700 * time.Second)
+	err := conn.Close()
+	if err != nil {
+		return
 	}
 }
