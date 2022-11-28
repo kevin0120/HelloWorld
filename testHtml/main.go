db1be5ed040814a8d204544c1536d66533be25f1
kevin0120 <jingwei1224@163.com>
2022-11-28 15:30:24 +0800

    report

--- testHtml/main.go
+++ testHtml/main.go
@@ -7,7 +7,31 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
+
+type Bolt struct {
+	Name      string
+	Precision float64
+	Recall    float64
+	Number    int
+}
+
+var Bolts = map[string]Bolt{
+	"OP1000-0-0": {
+		Name:      "OP1000-0-0",
+		Precision: 100.00,
+		Recall:    83.33,
+		Number:    403,
+	},
+
+	"H170-0-0": {
+		Name:      "H170-0-0",
+		Precision: 92.31,
+		Recall:    92.31,
+		Number:    746,
+	},
+}
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	t1, err := template.ParseFiles("./testHtml/template/hello.html")
@@ -27,12 +51,27 @@ func report(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s := strings.Split(r.RequestURI, "/")
-	par := "hello world"
+	par := "H170-0-0"
 	if len(s) >= 3 {
 		par = s[2]
 	}
 
-	err = t1.Execute(w, par)
+	expiration := time.Now()
+	expiration = expiration.AddDate(1, 0, 0)
+	cookie := http.Cookie{
+		Name:    "username",
+		Value:   "nill",
+		Expires: expiration,
+	}
+	http.SetCookie(w, &cookie)
+
+	var bolt Bolt
+	bolt = Bolts["H170-0-0"]
+	if _, ok := Bolts[par]; ok {
+		bolt = Bolts[par]
+	}
+
+	err = t1.Execute(w, bolt)
 	if err != nil {
 		return
 	}
